37f99730f92e35fccae0c5f67bac2eaa39737f1d
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2022-05-29 20:15:41 -0500

    <Infrastructure>[]: <Bump python  ⬆️ >
    [ ]
    
    [https://github.com/hasii2011/traviscli/issues/2]

--- travisci/TravisCli.py
+++ travisci/TravisCli.py
@@ -260,7 +260,7 @@ class TravisCli:
 @option('--major-version',     required=False, type=INT, help='Change the major number to the specified one')
 @option('--minor-version',     required=False, type=INT, help='Change the minor number to the specified one')
 @option('--patch-version',     required=False, type=INT, help='Change the patch number to the specified one')
-@version_option(version='0.3.0', message='%(version)s')
+@version_option(version='0.3.2', message='%(version)s')
 def commandHandler(build_count: int, repo_slug: str, file: TextIO, major_version: int, minor_version: int, patch_version: int):
     """
     Use this command to get the Travis CI build number of your project.  Assumes you are using Semantic Versioning
